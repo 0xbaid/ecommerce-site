3ea340a1bfbd0ce43b108d7b57eb8d3cb44601c9
Obaid <obaidulhaq20@gmail.com>
2020-03-21 02:06:57 +0500

    Setup the shop page with items in it

---
+++ src/pages/shop/shop.component.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './shop.component.style.scss';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+    render() {
+        const {collections} = this.state;
+        return (
+            <div>
+                {collections.map(({id, ...otherCollectionPorps})=>(
+                    <CollectionPreview key={id} {...otherCollectionPorps} />
+                ))}           
+            </div>
+        );
+    }
+}
+
+export default ShopPage;

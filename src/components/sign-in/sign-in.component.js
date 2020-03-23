124baf6979c4fff9f9c362a85c61fd67540fd6b3
Obaid <obaidulhaq20@gmail.com>
2020-03-23 21:34:35 +0500

    integrated firebase authentication with signInWithGoogle and signOut

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -50,9 +50,10 @@ class SignIn extends Component {
                     value={this.state.password} 
                     handleChange={this.handleChange} 
                     required /> 
-
+                <div className='buttons'>
                     <CustomButton type='submit'> Sign In </CustomButton> 
-                    <CustomButton onClick={signInWithGoogle}> Sign In With Google </CustomButton> 
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In With Google </CustomButton> 
+                </div>
                 </form>                
             </div>
         );

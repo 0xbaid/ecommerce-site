19409db163af28b17b0cf9bcf37b021fd5b01385
Obaid <obaidulhaq20@gmail.com>
2020-03-31 19:05:42 +0500

    added cart utils file to group items in cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { cartActionTypes } from '../cart/cart.action.types';
+import {addItemsToCart} from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state=INITIAL_STATE, action) => {
         case cartActionTypes.ADD_ITEM:
             return{
                 ...state,
-                cartItems : [...state.cartItems, action.payload]
+                cartItems: addItemsToCart(state.cartItems, action.payload)
             }
         default:
             return state;

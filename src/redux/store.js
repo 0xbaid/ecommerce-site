1fe2facdfa96e6785506a3b455c0040d346c03ce
Obaid <obaidulhaq20@gmail.com>
2020-04-03 21:03:40 +0500

    Implemented redux persist to store cart state locally in browser

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,9 @@ import {createStore, applyMiddleware} from 'redux';
 import  logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middleWares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+import { persistStore } from 'redux-persist';
 
-export default store;
+const middleWares = [logger];
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const persistor = persistStore(store);
+
